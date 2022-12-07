3351f814317c686c7d8276d5b1f40341a3fb7ff7
ywCN <wangyouhao90@gmail.com>
2022-12-07 15:58:33 -0500

    add description

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,5 +1,9 @@
-class MaximumSubarray {
+// tag: array, 
+// source: https://leetcode.com/problems/maximum-subarray/
+// description: Given an integer array nums, find the subarray which has the largest sum and return its sum.
+// example: maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) => 6 because [4,-1,2,1] has the largest sum = 6
 
+class MaximumSubarray {
 
   public static void main(String[] args) {
     System.out.print("test test");
