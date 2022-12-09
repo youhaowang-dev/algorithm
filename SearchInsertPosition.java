8e8213dee6025936a03dfdad0c2ad237ea91ebc8
ywCN <wangyouhao90@gmail.com>
2022-12-09 18:31:56 -0500

    searchInsertPosition

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -10,12 +10,67 @@
 
 final class SearchInsertPosition {
 
+  // find the biggest number smaller than target, return its position+1
+  public int searchInsertPosition(int[] nums, int target) {
+    if (nums.length == 0) {
+      return 0;
+    }
+
+    int start = 0;
+    int end = nums.length - 1;
+    while (start + 1 < end) {
+      int mid = start - (start - end) / 2;
+      int midNum = nums[mid];
+      if (midNum == target) {
+        return mid;
+      }
+      if (midNum > target) {
+        end = mid;
+      }
+      if (midNum < target) {
+        start = mid;
+      }
+    }
+
+    if (start == end) {
+      if (nums[start] == target) {
+        return start;
+      }
+      if (nums[start] > target) {
+        return start;
+      }
+      if (nums[start] < target) {
+        return start + 1;
+      }
+    }
+
+    if (start < end) { // or start + 1 == end
+      if (target < nums[start]) {
+        return start;
+      }
+      if (target == nums[start]) {
+        return start;
+      }
+      if (target > nums[start] && target < nums[end]) {
+        return end;
+      }
+      if (target == nums[end]) {
+        return end;
+      }
+      if (target > nums[end]) {
+        return end + 1;
+      }
+    }
+
+    return Integer.MAX_VALUE; // should not reach this line; TODO: throw
+  }
+
   public static void main(String[] args) throws Exception {
     SearchInsertPosition SearchInsertPosition = new SearchInsertPosition();
     int[] nums = new int[] { 1, 3, 5, 6 };
     int[] testCases = new int[] { 2, 5, 7 };
     // NOTE: the method must be public to make .getMethod work
-    String[] testMethodNames = new String[] {};
+    String[] testMethodNames = new String[] { "searchInsertPosition" };
 
     for (int target : testCases) {
       for (String methodName : testMethodNames) {
