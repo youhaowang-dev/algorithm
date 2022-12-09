643c3008cf7c638bf6ead2a070c6952544e0e73c
ywCN <wangyouhao90@gmail.com>
2022-12-08 20:52:25 -0500

    add max heap solution

--- KthLargestElementInAnArray.java
+++ KthLargestElementInAnArray.java
@@ -8,16 +8,47 @@
 // example: nums = [3,2,1,5,6,4], k = 2, output = 5
 //          nums = [3,2,3,1,2,4,5,5,6], k = 4, output = 4
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*; // write an assumption for this in interview
 
 class KthLargestElementInAnArray {
 
-  public int findKthLargestQuickSort(int[] nums, int k) {
+  public int findKthLargestByQuickSort(int[] nums, int k) {
     (new QuickSort()).quickSort(nums, 0, nums.length - 1);
 
     return nums[nums.length - 1 - (k - 1)];
+  }
+
+  // The idea is to init a heap "the smallest element first", and add all elements
+  // from the array into this heap one by one keeping the size of the heap always
+  // less or equal to k. That would results in a heap containing k largest elements of the array.
+  // The head of this heap is the answer, i.e. the kth largest element of the array.
+  public int findKthLargestByHeap(int[] nums, int k) {
+    // Comparator maxHeapComparator = new Comparator<Integer>() {
+    //   @Override
+    //   public int compare(Integer num1, Integer num2) {
+    //     return Integer.compare(num1, num2); // for min heap, use (num2, num1)
+    //   }
+    // };
+
+    // PriorityQueue<Integer> topKNums = new PriorityQueue<Integer>(
+    //   maxHeapComparator
+    // );
+
+    // Queue<Integer> topKNums = new PriorityQueue<Integer>((num1, num2) -> {
+    //   return num1 - num2;
+    // });
+
+    Queue<Integer> topKNums = new PriorityQueue<Integer>((num1, num2) ->
+      num1 - num2
+    );
+    for (int num : nums) {
+      topKNums.add(num);
+      if (topKNums.size() > k) {
+        topKNums.poll();
+      }
+    }
+
+    return topKNums.peek();
   }
 
   static class Params {
@@ -41,16 +72,20 @@ class KthLargestElementInAnArray {
       ),
     };
     // NOTE: the method must be public to make .getMethod work
-    String[] testMethodNames = new String[] { "findKthLargestQuickSort" };
+    String[] testMethodNames = new String[] {
+      "findKthLargestByQuickSort",
+      "findKthLargestByHeap",
+    };
 
     for (Params params : testCases) {
       for (String methodName : testMethodNames) {
         int[] numsCopy = Arrays.copyOf(params.nums, params.nums.length);
         System.out.println(
           String.format(
-            "Method Name: %s\nInput: %s",
+            "Method Name: %s\nInput Array: %s, K: %s",
             methodName,
-            Arrays.toString(numsCopy)
+            Arrays.toString(numsCopy),
+            params.k
           )
         );
         java.lang.reflect.Method method = kthLargestElementInAnArray
