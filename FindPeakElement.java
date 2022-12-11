6270543497e22442c21c074ede2ad1014a61c963
ywCN <wangyouhao90@gmail.com>
2022-12-10 22:07:45 -0500

    test case

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -20,5 +20,36 @@
 // Input: nums = [1,2,1,3,5,6,4]
 // Output: 5
 // Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+import java.util.*;
 
-final class FindPeakElement {}
+final class FindPeakElement {
+
+  public int findPeakElement(int[] nums) {
+    return -1;
+  }
+
+  public static void main(String[] args) throws Exception {
+    FindPeakElement FindPeakElement = new FindPeakElement();
+    int[][] arrays = new int[][] { { 1, 2, 3, 1 }, { 1, 2, 1, 3, 5, 6, 4 } };
+    // NOTE: the method must be public to make .getMethod work
+    String[] testMethodNames = new String[] { "findPeakElement" };
+
+    for (int[] array : arrays) {
+      for (String methodName : testMethodNames) {
+        java.lang.reflect.Method method = FindPeakElement
+          .getClass()
+          .getMethod(methodName, int[].class);
+        int index = (int) method.invoke(FindPeakElement, array);
+
+        System.out.println(
+          String.format(
+            "Method Name: %s\nInput: %s, Output: %s",
+            methodName,
+            Arrays.toString(array),
+            index
+          )
+        );
+      }
+    }
+  }
+}
