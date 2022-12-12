1097f91b46248818afaad27fe277fc059cc42d02
ywCN <wangyouhao90@gmail.com>
2022-12-11 21:08:20 -0500

    add class

---
+++ RotateString.java
@@ -0,0 +1,18 @@
+// String, String Matching
+// Amazon 3 Apple 2 Goldman Sachs 3 Zoom 3 LinkedIn 2 Microsoft 2 Oracle 2 Cisco 2 Bloomberg 3 Adobe 3 Google 2 Yahoo 2 Morgan Stanley 2
+// https://leetcode.com/problems/rotate-string/
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+// For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+// Example 1:
+// Input: s = "abcde", goal = "cdeab"
+// Output: true
+
+// Example 2:
+// Input: s = "abcde", goal = "abced"
+// Output: false
+
+final class RotateString {}
