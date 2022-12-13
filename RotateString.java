a9057c8682610dd587f8cfcfb4f7628a06b63d5e
ywCN <wangyouhao90@gmail.com>
2022-12-12 19:35:12 -0500

    add solution

--- RotateString.java
+++ RotateString.java
@@ -15,4 +15,48 @@
 // Input: s = "abcde", goal = "abced"
 // Output: false
 
-final class RotateString {}
+final class RotateString {
+
+  // https://leetcode.com/problems/rotate-string/solutions/678931/best-solution-for-an-interview-java-thoughts-and-takeaways/?orderBy=most_votes
+
+  // More specifically, say we rotate A by s.
+  // Then, instead of A[0], A[1], A[2], ..., we have A[s], A[s+1], A[s+2], ...;
+  // and we should check that A[s] == B[0], A[s+1] == B[1], A[s+2] == B[2], etc.
+  public boolean rotateString(String A, String B) {
+    if (A == null || B == null) {
+      // throw?
+      return false;
+    }
+    if (A.length() != B.length()) {
+      return false;
+    }
+    int length = A.length();
+    if (length == 0) {
+      return true;
+    }
+    for (int rotation = 0; rotation < length; rotation++) {
+      if (isRotated(A, B, rotation)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isRotated(String A, String B, int rotation) {
+    int length = A.length();
+    for (int i = 0; i < length; i++) {
+      int bIndex = (i + rotation) % length;
+      if (A.charAt(i) != B.charAt(bIndex)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // All rotations of A are contained in A+A. Thus, we can simply check
+  // whether B is a substring of A+A. We also need to check A.length == B.length,
+  // otherwise we will fail cases like A = "a", B = "aa".
+  public boolean rotateString2(String A, String B) {
+    return A.length() == B.length() && (A + A).contains(B);
+  }
+}
