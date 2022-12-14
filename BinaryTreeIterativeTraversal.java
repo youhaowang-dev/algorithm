087d5fa6bb5909aa43fdd62361c13569128a7021
ywCN <wangyouhao90@gmail.com>
2022-12-14 16:16:55 -0500

    add summary class

---
+++ BinaryTreeIterativeTraversal.java
@@ -0,0 +1,30 @@
+// summary of iterative solutions for preorder, inorder, and postorder
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.*;
+
+class BinaryTreeIterativeTraversal {
+
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+  }
+
+  public List<Integer> inorderTraversal(TreeNode root) {}
+
+  public List<Integer> postorderTraversal(TreeNode root) {}
+}
