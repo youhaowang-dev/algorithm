b848e2acc864672bc9d91be3369809398e30e55e
ywCN <wangyouhao90@gmail.com>
2022-12-14 15:39:19 -0500

    add class

---
+++ BinaryTreePostorderTraversal.java
@@ -0,0 +1,35 @@
+// Stack, Tree, Depth-First Search, Binary Tree
+// Facebook 2 Amazon 2 Apple 2 TuSimple 2 Bloomberg 2
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.*;
+
+class BinaryTreePostorderTraversal {
+
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+  }
+
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+  }
+
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+  }
+}
