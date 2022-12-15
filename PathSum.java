9a99627edca0793274c63384a6d65fa2fd7fe6f5
ywCN <wangyouhao90@gmail.com>
2022-12-15 14:38:48 -0500

    add class

---
+++ PathSum.java
@@ -0,0 +1,9 @@
+// Tree, Depth-First Search, Breadth-First Search, Binary Tree
+// Amazon 6 Google 3 Bloomberg 2 Microsoft 8 Facebook 7 Walmart Global Tech 2
+// Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+// A leaf is a node with no children.
+class PathSum {
+
+  public boolean hasPathSum(TreeNode root, int targetSum) {}
+}
