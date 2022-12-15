883d8f51076051e18129464ca289ff0c0eb5734e
ywCN <wangyouhao90@gmail.com>
2022-12-14 19:44:50 -0500

    add class

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,29 @@
+// Amazon 4 Google 3 Adobe 3 Spotify 3 Facebook 2 Uber 2 Bloomberg 2
+// Tree, Depth-First Search, Binary Tree
+
+// https://leetcode.com/problems/balanced-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class BalancedBinaryTree {
+
+  public boolean isBalanced(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+  }
+}
