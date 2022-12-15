c72db9be56d2b46256bf9de107f96904b5d72d65
ywCN <wangyouhao90@gmail.com>
2022-12-14 21:39:38 -0500

    add class

---
+++ LowestCommonAncestorOfABinaryTreeII.java
@@ -0,0 +1,23 @@
+// Tree, Depth-First Search, Binary Tree
+// Facebook 2 Microsoft 2 Amazon 2 LinkedIn
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-ii/
+// Given the root of a binary tree, return the lowest common ancestor (LCA) of two given nodes,
+// p and q. If either node p or q does not exist in the tree, return null. All values of the nodes in the tree are unique.
+
+// According to the definition of LCA on Wikipedia: "The lowest common ancestor of two nodes p and q in a
+// binary tree T is the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)".
+// A descendant of a node x is a node y that is on the path from node x to some leaf node.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class LowestCommonAncestorOfABinaryTreeII {
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {}
+}
