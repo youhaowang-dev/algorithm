e972c4c3e840750f83d931a31dcdd9cfdaf17090
ywCN <wangyouhao90@gmail.com>
2022-12-15 14:39:17 -0500

    node

--- PathSum.java
+++ PathSum.java
@@ -4,6 +4,22 @@
 // Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
 
 // A leaf is a node with no children.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class PathSum {
 
   public boolean hasPathSum(TreeNode root, int targetSum) {}
