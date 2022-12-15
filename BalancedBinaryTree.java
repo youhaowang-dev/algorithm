bb8576e4c7b00c31001fdaae4cd4ed5d8ed292e2
ywCN <wangyouhao90@gmail.com>
2022-12-14 19:53:10 -0500

    add solution

--- BalancedBinaryTree.java
+++ BalancedBinaryTree.java
@@ -22,8 +22,37 @@
 class BalancedBinaryTree {
 
   public boolean isBalanced(TreeNode root) {
-    if (root == null) {
-      return true;
+    return this.evaluateDepth(root).isBalanced;
+  }
+
+  private class Result {
+
+    int depth;
+    boolean isBalanced;
+
+    public Result(int depth, boolean isBalanced) {
+      this.depth = depth;
+      this.isBalanced = isBalanced;
     }
+  }
+
+  private Result evaluateDepth(TreeNode root) {
+    if (root == null) {
+      return new Result(0, true);
+    }
+
+    Result leftResult = this.evaluateDepth(root.left);
+    Result rightResult = this.evaluateDepth(root.right);
+
+    if (!leftResult.isBalanced || !rightResult.isBalanced) {
+      return new Result(0, false);
+    }
+
+    if (Math.abs(leftResult.depth - rightResult.depth) > 1) {
+      return new Result(0, false);
+    }
+
+    // dont forget +1
+    return new Result(Math.max(leftResult.depth, rightResult.depth) + 1, true);
   }
 }
