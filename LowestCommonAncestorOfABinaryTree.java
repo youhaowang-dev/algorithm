b41da0b2bb6c1d1b821720da5d132da2b5998adb
ywCN <wangyouhao90@gmail.com>
2022-12-14 20:00:08 -0500

    add class

---
+++ LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,26 @@
+// Tree, Depth-First Search, Binary Tree
+// Facebook 24 Amazon 22 Bloomberg 8 Microsoft 4 Karat 4 Google 2 LinkedIn 2 Oracle 2 Yahoo 2 Splunk 2 Qualcomm 2 TikTok 2 Samsung 2
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between
+// two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+// p and q will exist in the tree.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class LowestCommonAncestorOfABinaryTree {
+
+  // find two paths from root to the given two nodes
+  // compare the paths in reverse order, find the node before first difference
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {}
+}
