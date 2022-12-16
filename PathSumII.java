bb0515a3a3c45e94ca98f6d288b3ce1b39d99b72
ywCN <wangyouhao90@gmail.com>
2022-12-15 20:25:26 -0500

    add class

---
+++ PathSumII.java
@@ -0,0 +1,28 @@
+// Backtracking, Tree, Depth-First Search, Binary Tree
+// Amazon 5 Bloomberg 3 DoorDash 3 Zillow 2
+// https://leetcode.com/problems/path-sum-ii/
+
+// Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+// A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class PathSumII {
+
+  public List<List<Integer>> pathSum(TreeNode root, int targetSum) {}
+}
