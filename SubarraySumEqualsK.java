5753c47315f8f19ca704c865174cd0a36cf075ff
ywCN <wangyouhao90@gmail.com>
2022-12-16 11:51:27 -0500

    add SubarraySumEqualsK

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,19 @@
+// Array, Hash Table, Prefix Sum
+// Amazon 15 Facebook 14 Microsoft 7 Google 6 Mathworks 5 Bloomberg 4 Yandex 4 Expedia 3 TikTok 3 Apple 2 Oracle 2 Citadel 2 Infosys 2 Bolt 2 DRW 2
+// https://leetcode.com/problems/subarray-sum-equals-k/
+
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1:
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+
+// Example 2:
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+class SubarraySumEqualsK {
+
+  public int subarraySum(int[] nums, int k) {}
+}
