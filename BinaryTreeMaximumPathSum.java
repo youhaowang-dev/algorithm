f0289ea9afcc5d789cfa3d3693cff2827b1b9851
ywCN <wangyouhao90@gmail.com>
2022-12-16 16:58:41 -0500

    add solution and comment

--- BinaryTreeMaximumPathSum.java
+++ BinaryTreeMaximumPathSum.java
@@ -27,5 +27,46 @@
 
 class BinaryTreeMaximumPathSum {
 
-  public int maxPathSum(TreeNode root) {}
+  private class PathVals {
+
+    int rootToAny;
+    int anyToAny;
+
+    public PathVals(int rootToAny, int anyToAny) {
+      this.rootToAny = rootToAny;
+      this.anyToAny = anyToAny;
+    }
+  }
+
+  // max(root to any) = max(left root to any, right root to any) + root.val
+  // max(any to any) = max(any to any, max left root to any + max right root to any + root.val)
+  // val can be negative, so is the max left/right/total, so max(0, ...) is needed
+  public int maxPathSum(TreeNode root) {
+    return this.getMaxPathSum(root).anyToAny;
+  }
+
+  private PathVals getMaxPathSum(TreeNode root) {
+    // exit
+    if (root == null) {
+      return new PathVals(Integer.MIN_VALUE, Integer.MIN_VALUE);
+    }
+
+    // divide
+    PathVals leftVals = this.getMaxPathSum(root.left);
+    PathVals rightVals = this.getMaxPathSum(root.right);
+
+    // merge
+    int maxRootToAny =
+      Math.max(0, Math.max(leftVals.rootToAny, rightVals.rootToAny)) + root.val;
+    int derivedAnyToAny =
+      Math.max(0, leftVals.rootToAny) +
+      Math.max(0, rightVals.rootToAny) +
+      root.val;
+    int maxAnyToAny = Math.max(
+      derivedAnyToAny,
+      Math.max(leftVals.anyToAny, rightVals.anyToAny)
+    );
+
+    return new PathVals(maxRootToAny, maxAnyToAny);
+  }
 }
