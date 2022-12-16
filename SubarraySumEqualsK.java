96f92acaaacf502c7ae1421d1a60bb57a711a9f1
ywCN <wangyouhao90@gmail.com>
2022-12-16 12:15:06 -0500

    add solution and comment

--- SubarraySumEqualsK.java
+++ SubarraySumEqualsK.java
@@ -15,5 +15,36 @@
 // Output: 2
 class SubarraySumEqualsK {
 
-  public int subarraySum(int[] nums, int k) {}
+  // There could be two situations. In situation 1, the subarray with the target sum starts from the beginning of
+  // the array. That means that the current prefix sum is equal to the target sum, and we increase the counter by 1.
+  // In situation 2, the subarray with the target sum starts somewhere in the middle. That means we should
+  // add to the counter the number of times we have seen the prefix sum curr_sum - target so far: count += h[curr_sum - target].
+  // The logic is simple: the current prefix sum is curr_sum, and some elements before
+  // the prefix sum was curr_sum - target. All the elements in between sum up to curr_sum - (curr_sum - target) = target.
+  public int subarraySum(int[] nums, int target) {
+    int count = 0;
+    Map<Integer, Integer> prefixSumToCount = new HashMap<>();
+
+    int prefixSum = 0;
+    for (int num : nums) {
+      prefixSum += num;
+      // situation 1
+      if (prefixSum == target) {
+        count++;
+      }
+
+      int previousTargetSum = prefixSum - target;
+      if (prefixSumToCount.containsKey(previousTargetSum)) {
+        count += prefixSumToCount.get(previousTargetSum);
+      }
+
+      if (prefixSumToCount.containsKey(prefixSum)) {
+        prefixSumToCount.put(prefixSum, prefixSumToCount.get(prefixSum) + 1);
+      } else {
+        prefixSumToCount.put(prefixSum, 1);
+      }
+    }
+
+    return count;
+  }
 }
