46f8517261814e9b1809615256f9b4d06263f26b
ywCN <wangyouhao90@gmail.com>
2022-12-16 16:04:26 -0500

    add desc

--- BinaryTreeMaximumPathSum.java
+++ BinaryTreeMaximumPathSum.java
@@ -2,6 +2,13 @@
 // DoorDash 23 Microsoft 5 Amazon 4 Facebook 3 Bloomberg 2 Adobe 2 Apple 2 Samsung 2 TikTok 2 Akuna Capital 2 Google 8 ByteDance 6 Snapchat 3 Oracle 2 Twitter 2 Twilio 2 Yandex 2 Sprinklr 2 TuSimple 2
 // https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
 
+// A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has
+// an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+// The path sum of a path is the sum of the node's values in the path.
+
+// Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
