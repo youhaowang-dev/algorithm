420e892899e01d2a0cd6d4a73372ec27d118ccca
ywCN <wangyouhao90@gmail.com>
2022-12-15 19:57:11 -0500

    recursive solution

--- PathSum.java
+++ PathSum.java
@@ -20,7 +20,23 @@
  *     }
  * }
  */
+
+// only check root-leaf
 class PathSum {
 
-  public boolean hasPathSum(TreeNode root, int targetSum) {}
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    if (root == null) {
+      return false;
+    }
+
+    targetSum = targetSum - root.val;
+    if (root.left == null && root.right == null) {
+      return targetSum == 0;
+    }
+
+    return (
+      this.hasPathSum(root.left, targetSum) ||
+      this.hasPathSum(root.right, targetSum)
+    );
+  }
 }
