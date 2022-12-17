fbb0822312644cd1e8f00b8547f876e4553ce6a5
ywCN <wangyouhao90@gmail.com>
2022-12-17 08:40:56 -0500

    add class

---
+++ InorderSuccessorInBST.java
@@ -0,0 +1,20 @@
+// Tree, Depth-First Search, Binary Search Tree, Binary Tree
+// Microsoft 3 Arista Networks 3 Facebook 5 Google 4 Adobe 3 Apple 2
+
+// Given the root of a binary search tree and a node p in it, return the in-order successor of that node in the BST. If the given node has no in-order successor in the tree, return null.
+
+// The successor of a node p is the node with the smallest key greater than p.val.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class InorderSuccessorInBST {
+
+  public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {}
+}
