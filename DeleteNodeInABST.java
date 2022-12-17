e4e11eba99f1042f5a51bc6f3cfd17ce6b728986
ywCN <wangyouhao90@gmail.com>
2022-12-17 13:22:03 -0500

    add class

---
+++ DeleteNodeInABST.java
@@ -0,0 +1,30 @@
+// Tree, Binary Search Tree, Binary Tree
+// Adobe 3 Amazon 2 Apple 2 Microsoft 5 Oracle 5 Google 3 LinkedIn 2 Bloomberg 2
+// https://leetcode.com/problems/delete-node-in-a-bst/
+
+// Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+// Basically, the deletion can be divided into two stages:
+
+// Search for a node to remove.
+// If the node is found, delete the node.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class DeleteNodeInABST {
+
+  public TreeNode deleteNode(TreeNode root, int key) {}
+}
