a1fec03ec9c984ce7865a171fdc579508cad2031
ywCN <wangyouhao90@gmail.com>
2022-12-17 18:19:31 -0500

    add class

---
+++ BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,25 @@
+// Tree, Breadth-First Search, Binary Tree
+// Bloomberg 11 Amazon 10 LinkedIn 2 Microsoft 8 Facebook 7 Oracle 4 Google 3 Apple 3 ServiceNow 3 Splunk 2 Adobe 2 Uber 3 Paypal 2 Visa 2 Accolite 2 TikTok 2
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreeLevelOrderTraversal {
+
+  public List<List<Integer>> levelOrder(TreeNode root) {}
+}
