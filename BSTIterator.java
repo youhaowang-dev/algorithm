2ddba941e7145eeac2ed20a0c1edfd5b24737c9f
ywCN <wangyouhao90@gmail.com>
2022-12-17 08:18:05 -0500

    add class

---
+++ BSTIterator.java
@@ -0,0 +1,41 @@
+// Stack, Tree, Design, Binary Search Tree, Binary Tree, Iterator
+// Facebook 7 Media.net 7 Microsoft 4 Amazon 4 Google 3 Bloomberg 3 Salesforce 2 Twilio 2
+
+// Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+// BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+// boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+// int next() Moves the pointer to the right, then returns the number at the pointer.
+// Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+// You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+  public BSTIterator(TreeNode root) {}
+
+  public int next() {}
+
+  public boolean hasNext() {}
+}
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
