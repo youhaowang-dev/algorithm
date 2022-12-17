2ae8f83573f65a0eed4fa49f26785ee2099302bd
ywCN <wangyouhao90@gmail.com>
2022-12-17 18:54:38 -0500

    add class

---
+++ BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,23 @@
+// Tree, Breadth-First Search, Binary Tree
+// Amazon 17 Bloomberg 8 Microsoft 7 Facebook 3 Adobe 3 Yandex 3 Apple 2 Oracle 2 Walmart Global Tech 2 LinkedIn 4 Google 2 ServiceNow 2 Salesforce 2 VMware 2 SAP 2 Cisco 2 TikTok 2
+// Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreeZigzagLevelOrderTraversal {
+
+  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {}
+}
