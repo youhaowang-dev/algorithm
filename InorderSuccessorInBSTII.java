2269aac3881998b3f8022fb64bcef8b8c009ab8e
ywCN <wangyouhao90@gmail.com>
2022-12-17 11:49:03 -0500

    add class

---
+++ InorderSuccessorInBSTII.java
@@ -0,0 +1,19 @@
+// Tree, Binary Search Tree, Binary Tree
+// Microsoft 4 Arista Networks 2 Facebook 2 Google 3 Bloomberg 3 Amazon 2 Akamai 2
+// https://leetcode.com/problems/inorder-successor-in-bst-ii/
+// Given a node in a binary search tree, return the in-order successor of that node in the BST. If that node has no in-order successor, return null.
+
+// The successor of a node is the node with the smallest key greater than node.val.
+
+// You will have direct access to the node but not to the root of the tree. Each node will have a reference to its parent node. Below is the definition for Node:
+
+// class Node {
+//     public int val;
+//     public Node left;
+//     public Node right;
+//     public Node parent;
+// }
+class InorderSuccessorInBSTII {
+
+  public Node inorderSuccessor(Node node) {}
+}
