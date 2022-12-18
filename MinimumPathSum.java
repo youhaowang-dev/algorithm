317928fcc45f4229d64a6b82fddc0cdd9093feb7
ywCN <wangyouhao90@gmail.com>
2022-12-18 18:38:30 -0500

    dp

--- MinimumPathSum.java
+++ MinimumPathSum.java
@@ -11,5 +11,35 @@
 
 class MinimumPathSum {
 
-  public int minPathSum(int[][] grid) {}
+  // dp
+  // state: minPathSums[row][col] is the min path sum from 0,0 to row,col
+  // function(row, col) = min(function(row - 1, col), function(row, col - 1))
+  public int minPathSum(int[][] grid) {
+    if (grid.length == 0 || grid[0].length == 0) {
+      return 0;
+    }
+
+    int rowCount = grid.length;
+    int colCount = grid[0].length;
+    int[][] minPathSums = new int[rowCount][colCount];
+    minPathSums[0][0] = grid[0][0];
+    for (int row = 1; row < rowCount; row++) {
+      minPathSums[row][0] = grid[row][0] + minPathSums[row - 1][0];
+    }
+    for (int col = 1; col < colCount; col++) {
+      minPathSums[0][col] = grid[0][col] + minPathSums[0][col - 1];
+    }
+
+    for (int row = 1; row < rowCount; row++) {
+      for (int col = 1; col < colCount; col++) {
+        int prevMinPathSum = Math.min(
+          minPathSums[row - 1][col],
+          minPathSums[row][col - 1]
+        );
+        minPathSums[row][col] = grid[row][col] + prevMinPathSum;
+      }
+    }
+
+    return minPathSums[rowCount - 1][colCount - 1];
+  }
 }
