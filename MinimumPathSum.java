8f5d759fe35d92997648a9f81dc0faa937bb3b44
ywCN <wangyouhao90@gmail.com>
2022-12-18 18:22:21 -0500

    add class

---
+++ MinimumPathSum.java
@@ -0,0 +1,15 @@
+// Array, Dynamic Programming, Matrix
+// Amazon 6 Google 4 Bloomberg 4 Microsoft 2 Goldman Sachs 2 AQR Capital Management LLC 2 Uber 5 Oracle 3 Square 3 Apple 2 Snapchat 2 ByteDance 2 Tesla 3 Adobe 2 Paypal 2 ServiceNow 2
+// https://leetcode.com/problems/minimum-path-sum/
+
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+// Note: You can only move either down or right at any point in time.
+
+// Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+// Output: 7
+// Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+class MinimumPathSum {
+
+  public int minPathSum(int[][] grid) {}
+}
