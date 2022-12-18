c22ad2497f382ead158ed0f8a16e7c7588fb22b5
ywCN <wangyouhao90@gmail.com>
2022-12-17 21:29:11 -0500

    add class

---
+++ BinaryTreeVerticalOrderTraversal.java
@@ -0,0 +1,26 @@
+// Hash Table, Tree, Depth-First Search, Breadth-First Search, Binary Tree
+// Facebook 32 Bloomberg 11 Amazon 2 Microsoft 4 Oracle 2 Salesforce 3 Apple 2 VMware 2 Google Snapchat
+
+// Given the root of a binary tree, return the vertical order traversal of its nodes' values. (i.e., from top to bottom, column by column).
+
+// If two nodes are in the same row and column, the order should be from left to right.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreeVerticalOrderTraversal {
+
+  public List<List<Integer>> verticalOrder(TreeNode root) {}
+}
