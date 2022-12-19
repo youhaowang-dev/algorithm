35a18f2074fd382bec3122d6573ef320047f88bc
ywCN <wangyouhao90@gmail.com>
2022-12-18 20:49:38 -0500

    add class

---
+++ JumpGame.java
@@ -0,0 +1,21 @@
+// Array, Dynamic Programming, Greedy
+// Amazon 20 Apple 5 Microsoft 5 Adobe 4 Google 4 Facebook 2 Bloomberg 2 Walmart Global Tech 2 Oracle 3 Salesforce 2 DoorDash 2 ByteDance 2 Flipkart 2 Docusign 2 Uber 4 Qualtrics 3 Goldman Sachs 3 TikTok 3 Paypal 2 Nutanix 2 Infosys 2 ShareChat 2
+// https://leetcode.com/problems/jump-game/
+// You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+// Return true if you can reach the last index, or false otherwise.
+
+// Example 1:
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+// Example 2:
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+class JumpGame {
+
+  public boolean canJump(int[] nums) {}
+}
---
+++ JumpGameII.java
@@ -0,0 +1,25 @@
+// Array, Dynamic Programming, Greedy
+// Amazon 10 Goldman Sachs 4 Bloomberg 3 Adobe 3 Apple 2 Google 2 Microsoft 2 Salesforce 2 DoorDash 2 PhonePe 2 ShareChat 4 Uber 2 Tesla 2 Snapchat 3 Facebook 3 Yahoo 3 Flipkart 3 TikTok 3 Morgan Stanley 2 Oracle 2 ByteDance 2 tcs 2 Citrix 2 Infosys 2 payu 2
+// https://leetcode.com/problems/jump-game-ii/
+
+// You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+// Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+// 0 <= j <= nums[i] and
+// i + j < n
+// Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+// Example 1:
+// Input: nums = [2,3,1,1,4]
+// Output: 2
+// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+// Example 2:
+// Input: nums = [2,3,0,1,4]
+// Output: 2
+
+class JumpGameII {
+
+  public int jump(int[] nums) {}
+}
