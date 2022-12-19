2a7e36d34491954a24a2254a349e7bb793958012
ywCN <wangyouhao90@gmail.com>
2022-12-19 09:19:42 -0500

    add class

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,23 @@
+// Array, Binary Search, Dynamic Programming
+// Google 7 Amazon 5 TikTok 5 Adobe 3 Apple 3 Infosys 2 TuSimple 2 Microsoft 10 Facebook 4 Bloomberg 4 Oracle 4 Expedia 3 VMware 2 Visa 2 Walmart Global Tech 2 ByteDance 7 Twitter 5 ServiceNow 3 Samsung 3 Salesforce 2 Splunk 2 Paypal 2 MakeMyTrip 2 ZScaler 2 Huawei 2 Shopee 2 Deutsche Bank 2 HRT 2
+// https://leetcode.com/problems/longest-increasing-subsequence/
+
+// Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+// Example 1:
+// Input: nums = [10,9,2,5,3,7,101,18]
+// Output: 4
+// Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+// Example 2:
+// Input: nums = [0,1,0,3,2,3]
+// Output: 4
+
+// Example 3:
+// Input: nums = [7,7,7,7,7,7,7]
+// Output: 1
+
+class LongestIncreasingSubsequence {
+
+  public int lengthOfLIS(int[] nums) {}
+}
