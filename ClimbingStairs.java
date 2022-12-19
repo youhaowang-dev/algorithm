6cf9f010fd8103bc248544632f8d456a4f8e8d6b
ywCN <wangyouhao90@gmail.com>
2022-12-18 20:24:31 -0500

    add class

---
+++ ClimbingStairs.java
@@ -0,0 +1,26 @@
+// Math, Dynamic Programming, Memoization
+// Amazon 16 Adobe 11 Google 5 Apple 5 Bloomberg 3 Microsoft 3 Uber 2 Visa 2 Nagarro 2 Yahoo 4 Oracle 4 Goldman Sachs 3 Expedia 3 Intel 2 Facebook 4 Walmart Global Tech 3 Morgan Stanley 3 LinkedIn 2 eBay 2 Cisco 2 VMware 2 Twilio 2 Optum 2
+
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+// Example 2:
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+class ClimbingStairs {
+
+  public int climbStairs(int n) {}
+}
