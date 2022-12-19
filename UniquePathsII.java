807bd3038de6b5ed5f9a21741cd5fd3067bb1537
ywCN <wangyouhao90@gmail.com>
2022-12-18 19:16:45 -0500

    add class

---
+++ UniquePathsII.java
@@ -0,0 +1,23 @@
+// Array, Dynamic Programming, Matrix
+// Amazon 9 Google 3 Facebook 2 Cruise Automation 8 Qualtrics 5 Cisco 3 Microsoft 2 Salesforce 2 Oracle 2 Athenahealth 2 Paypal 2 Bloomberg 3 Goldman Sachs 2 Apple 2 Zillow 2 ByteDance 2 Intuit 2
+// https://leetcode.com/problems/unique-paths-ii/
+
+// You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+// Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+// Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+// Output: 2
+// Explanation: There is one obstacle in the middle of the 3x3 grid above.
+// There are two ways to reach the bottom-right corner:
+// 1. Right -> Right -> Down -> Down
+// 2. Down -> Down -> Right -> Right
+
+class UniquePathsII {
+
+  public int uniquePathsWithObstacles(int[][] grid) {}
+}
