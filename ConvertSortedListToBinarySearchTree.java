fd2b854137875bdc410d9d97fc54d293c23b4f7a
ywCN <wangyouhao90@gmail.com>
2022-12-20 14:49:27 -0500

    solution

--- ConvertSortedListToBinarySearchTree.java
+++ ConvertSortedListToBinarySearchTree.java
@@ -39,5 +39,39 @@
  */
 class ConvertSortedListToBinarySearchTree {
 
-  public TreeNode sortedListToBST(ListNode head) {}
+  public TreeNode sortedListToBST(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode mid = this.findMidAndDisconnectPrev(head);
+
+    TreeNode node = new TreeNode(mid.val); // root
+
+    // edge case and exit condition; when list has only one node, add the tree node
+    if (head == mid) {
+      return node;
+    }
+
+    node.left = this.sortedListToBST(head);
+    node.right = this.sortedListToBST(mid.next);
+
+    return node;
+  }
+
+  private ListNode findMidAndDisconnectPrev(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    ListNode prevSlow = null;
+    while (fast != null && fast.next != null) {
+      prevSlow = slow;
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    if (prevSlow != null) {
+      prevSlow.next = null;
+    }
+
+    return slow;
+  }
 }
