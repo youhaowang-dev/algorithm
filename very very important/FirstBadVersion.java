a02d769ead3ef4f58dbe7688ca79e3f7264e8f85
ywCN <wangyouhao90@gmail.com>
2022-12-30 16:23:18 -0500

    FirstBadVersion

--- very very important/FirstBadVersion.java
+++ very very important/FirstBadVersion.java
@@ -1,6 +1,6 @@
-// tag: Binary Search, Interactive
-// Google 10 Amazon 8 Adobe 7 Apple 6 Facebook 3 Microsoft 3 Uber 2
-
+// Binary Search, Interactive
+// Google 8 Amazon 8 Adobe 7 Apple 6 Microsoft 3 Facebook 2 Uber 2 Yahoo 2 VMware 2 Bloomberg 2 Expedia 2 Cisco 2 Oracle 2 Goldman Sachs 2
+// https://leetcode.com/problems/first-bad-version/
 // You are a product manager and currently leading a team to develop a new product.
 // Unfortunately, the latest version of your product fails the quality check.
 // Since each version is developed based on the previous version, all the versions after a bad version are also bad.
@@ -24,40 +24,46 @@
 // Input: n = 1, bad = 1
 // Output: 1
 
+// 1,2,3,4,5,6
+//     ^ 3 is first bad version
+// l   m     r => m is bad version, drop right side [m+1, r] because target(first bad version) is not in this part
+// l   r
+// lm  r  => m is not bad, drop [l, m] as this part does not have the target(first bad version)
+//   l r  => l=1, r=2, check left, if bad return, otherwise return right
 final class FirstBadVersion {
 
   // TLE on leetcode but same complexity
   public int firstBadVersion(int n) {
-    int start = 1;
-    int end = n;
-    while (start + 1 < end) {
-      int mid = start - (start - end) / 2;
+    int left = 1;
+    int right = n;
+    while (left + 1 < right) {
+      int mid = left - (left - right) / 2;
       if (isBadVersion(mid)) {
-        end = mid;
+        right = mid;
       } else {
-        start = mid + 1;
+        left = mid + 1;
       }
     }
-    if (isBadVersion(start)) {
-      return start;
+    if (isBadVersion(left)) {
+      return left;
     }
 
-    return end;
+    return right;
   }
 
   public int firstBadVersionV2(int n) {
-    int start = 1;
-    int end = n;
-    while (start < end) {
-      int mid = start - (start - end) / 2;
+    int left = 1;
+    int right = n;
+    while (left < right) {
+      int mid = left - (left - right) / 2;
       if (isBadVersion(mid)) {
-        end = mid;
+        right = mid;
       } else {
-        start = mid + 1;
+        left = mid + 1;
       }
     }
 
-    // start == end
-    return start;
+    // left == right
+    return left;
   }
 }
