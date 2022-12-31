27671f231be9908151ba820beb5a09bba0f4d491
ywCN <wangyouhao90@gmail.com>
2022-12-30 19:23:46 -0500

    add info

--- very very important/MinimumDepthofBinaryTree.java
+++ very very important/MinimumDepthofBinaryTree.java
@@ -1,3 +1,33 @@
+// Tree, Depth-First Search, Breadth-First Search, Binary Tree
+// Amazon 3 Facebook 3 Bloomberg 2 Goldman Sachs 3 Adobe 2
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+// Given a binary tree, find its minimum depth.
+
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+// Note: A leaf is a node with no children.
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 2
+// Example 2:
+// Input: root = [2,null,3,null,4,null,5,null,6]
+// Output: 5
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class MinimumDepthofBinaryTree {
 
   // recursive dfs
