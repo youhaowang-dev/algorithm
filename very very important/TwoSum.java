a01d1adb4c88383234c7375b299c7ff141461e81
Youhao Wang <youhaowang.dev@gmail.com>
2023-01-02 08:12:30 -0500

    2 sum

--- very very important/TwoSum.java
+++ very very important/TwoSum.java
@@ -2,19 +2,17 @@ class TwoSum {
 
   // O(n) space and time
   public int[] twoSum(int[] nums, int target) {
-    HashMap<Integer, Integer> numToIndex = new HashMap<>();
-
+    Map<Integer, Integer> numToIndex = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
       int num = nums[i];
-      int otherNum = target - num;
-      if (numToIndex.containsKey(otherNum)) {
-        return new int[] { i, numToIndex.get(otherNum) };
-      } else {
-        numToIndex.put(num, i);
+      int targetRemain = target - num;
+      if (numToIndex.containsKey(targetRemain)) {
+        return new int[] { i, numToIndex.get(targetRemain) };
       }
+      numToIndex.put(num, i);
     }
 
-    return new int[] { 0, 0 };
+    return new int[] { -1, -1 };
   }
 
   // brute force
