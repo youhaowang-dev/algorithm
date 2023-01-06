0043b717737b832cb2d0034b747f68ebe72632b7
Youhao Wang <youhaowang.dev@gmail.com>
2023-01-06 15:16:18 -0500

    reverse 3 digits

---
+++ P2/Reverse3digitInteger.py
@@ -0,0 +1,28 @@
+# https://www.lintcode.com/problem/37/
+# Reverse a 3-digit integer.
+# Example 1:
+# Input:
+# number = 123
+# Output: 321
+# Explanation:
+# Reverse the number.
+# Example 2:
+# Input:
+# number = 900
+# Output: 9
+# Explanation:
+# Reverse the number.
+
+
+class Reverse3digitInteger:
+    def reverse_integer(self, number: int) -> int:
+        digit1 = number % 10
+        number = number // 10
+
+        digit2 = number % 10
+        number = number // 10
+
+        digit3 = number % 10
+        number = number // 10
+
+        return 100 * digit1 + 10 * digit2 + 1 * digit3
