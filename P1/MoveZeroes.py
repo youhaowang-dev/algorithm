a08c346c65f314d781f2ed267196c00acf8d1436
Youhao Wang <youhaowang.dev@gmail.com>
2023-01-08 14:26:53 -0500

    move zeros

--- P1/MoveZeroes.py
+++ P1/MoveZeroes.py
@@ -28,7 +28,29 @@ class MoveZeroes:
         for i in range(0, len(nums)):
             if nums[i] == self.ZERO:
                 zero_count += 1
+                print(zero_count)
 
-            if zero_count != self.ZERO:
+            # if zero_count != self.ZERO: # this wont work
+            else:
                 zero_index = i - zero_count
-                (nums[i], nums[zero_index]) = (nums[zero_index], nums[i])
+                (nums[zero_index], nums[i]) = (nums[i], nums[zero_index])
+
+
+# O(n) less writes
+class MoveZeroes:
+    ZERO = 0
+
+    def moveZeroes(self, nums: List[int]) -> None:
+        last_non_zero = 0
+        # If the current element is not 0, then we need to
+        # append it just in front of last non 0 element we found.
+        for i in range(0, len(nums)):
+            if nums[i] != 0:
+                nums[last_non_zero] = nums[i]
+                last_non_zero += 1
+
+        # After we have finished processing new elements,
+        # all the non-zero elements are already at beginning of array.
+        # We just need to fill remaining array with 0's.
+        for i in range(last_non_zero, len(nums)):
+            nums[i] = 0
