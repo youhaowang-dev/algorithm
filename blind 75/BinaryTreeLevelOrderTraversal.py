c945c601bdf4bde4b1a6bb9d0927239073b36278
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-05 10:41:20 -0500

    move file

--- blind 75/BinaryTreeLevelOrderTraversal.py
+++ blind 75/BinaryTreeLevelOrderTraversal.py
@@ -1,50 +1,38 @@
 # Tree, Breadth-First Search, Binary Tree
-# Bloomberg 11 Amazon 10 LinkedIn 2 Microsoft 8 Facebook 7 Oracle 4 Google 3 Apple 3 ServiceNow 3 Splunk 2 Adobe 2 Uber 3 Paypal 2 Visa 2 Accolite 2 TikTok 2
+# Bloomberg 11 Amazon 10 LinkedIn 2 Microsoft 8 Facebook 7 Oracle 4 Google 3 Apple 3 ServiceNow 3 Splunk 2 Adobe 2
+# Uber 3 Paypal 2 Visa 2 Accolite 2 TikTok 2
 # https://leetcode.com/problems/binary-tree-level-order-traversal/
 
 # Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
 
 
-from collections import deque
-from typing import List, Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 class BinaryTreeLevelOrderTraversal:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        result = list()
+        results = list()
         if not root:
-            return result
+            return results
 
         queue = deque()
         queue.append(root)
 
         while queue:
-            current_level_nodes = self.getCurrent_level_nodes(queue)
-            current_level_result = list()
-            for node in current_level_nodes:
-                current_level_result.append(node.val)
+            nodes = self.get_all_nodes(queue)
+            result = list()
+            for node in nodes:
+                result.append(node.val)
 
                 if node.left:
                     queue.append(node.left)
-
                 if node.right:
                     queue.append(node.right)
 
-            result.append(current_level_result)
+            results.append(result)
 
-        return result
+        return results
 
-    def getCurrent_level_nodes(self, queue) -> List[int]:
+    def get_all_nodes(self, queue) -> List[TreeNode]:
         nodes = list()
-
-        while len(queue) > 0:
+        while queue:
             nodes.append(queue.popleft())
 
         return nodes
