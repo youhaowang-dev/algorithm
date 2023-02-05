61e37d5267cf4a9858f27f30b8c36f61b4720f9d
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-04 21:31:04 -0500

    min depth

--- blind 75/MaximumDepthOfBinaryTree.py
+++ blind 75/MaximumDepthOfBinaryTree.py
@@ -23,3 +23,16 @@ class MaximumDepthOfBinaryTree:
             return 1 + max(self.minDepth(root.left), self.minDepth(root.right))
         else:  # leaf
             return 1
+
+    def minDepth2(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = collections.deque([(root, 1)])
+        while queue:
+            node, level = queue.popleft()
+            if node:
+                if not node.left and not node.right:
+                    return level
+                else:
+                    queue.append((node.left, level+1))
+                    queue.append((node.right, level+1))
