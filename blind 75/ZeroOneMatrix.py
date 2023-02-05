23b82957a8d33ad7f1e614002b69bc44d2ab25d6
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-05 09:44:33 -0500

    ZeroOneMatrix

--- blind 75/ZeroOneMatrix.py
+++ blind 75/ZeroOneMatrix.py
@@ -22,17 +22,13 @@
 #          [0,1,0],
 #          [1,2,1]]
 
-from collections import deque
-import math
-from typing import List
-
-# bfs
-# https://leetcode.com/problems/01-matrix/solutions/1369741/c-java-python-bfs-dp-solutions-with-picture-clean-concise-o-1-space
-# Same idea with Topology Sort, we process zero-cells first, then we use queue data structure to keep the order
-# of processing cells, so that cells which have the smaller distance will be processed first.
+# bfs, process from 0 distance to more distance cells like topologic sort
+# current distance = neighbor distance + 1
+# 0, 0, 0            0, 0, 0          0, 0, 0           0, 0, 0        0, 0, 0
+# 0, -1, 0     =>    0, 1, 0    =>    0, 1, 0     =>    0, 1, 0   =>   0, 1, 0
+# -1, -1, -1        -1, -1, -1        1, -1, -1         1, -1, 1       1, 2, 1
 class ZeroOneMatrix:
-    DIRECTION_DELTAS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    TO_BE_PROCESSED = -1
+    WAS_ONE = -1  # distance can be 0,1,2,3... so -1 won't be used
 
     def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
         if not matrix or not matrix[0]:
@@ -46,12 +42,12 @@ class ZeroOneMatrix:
             for col in range(col_count):
                 if matrix[row][col] == 0:
                     queue.append((row, col))
-                else:
-                    matrix[row][col] = self.TO_BE_PROCESSED
+                elif matrix[row][col] == 1:
+                    matrix[row][col] = self.WAS_ONE
 
-        while queue:
+        while queue:  # bfs, matrix[was one] = matrix[prev distance] + 1
             row, col = queue.popleft()
-            for row_delta, col_delta in self.DIRECTION_DELTAS:
+            for row_delta, col_delta in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 new_row = row + row_delta
                 new_col = col + col_delta
                 if (
@@ -59,11 +55,13 @@ class ZeroOneMatrix:
                     or new_row >= row_count
                     or new_col < 0
                     or new_col >= col_count
-                    or matrix[new_row][new_col] != self.TO_BE_PROCESSED
                 ):
                     continue
 
-                matrix[new_row][new_col] = matrix[row][col] + 1
+                if matrix[new_row][new_col] != self.WAS_ONE:
+                    continue
+
+                matrix[new_row][new_col] = 1 + matrix[row][col]
                 queue.append((new_row, new_col))
 
         return matrix
@@ -88,8 +86,11 @@ class ZeroOneMatrix2:
         for row in range(row_count - 1, -1, -1):
             for col in range(col_count - 1, -1, -1):
                 if matrix[row][col] > 0:
-                    bottom = matrix[row + 1][col] if row < row_count - 1 else math.inf
-                    right = matrix[row][col + 1] if col < col_count - 1 else math.inf
-                    matrix[row][col] = min(matrix[row][col], bottom + 1, right + 1)
+                    bottom = matrix[row +
+                                    1][col] if row < row_count - 1 else math.inf
+                    right = matrix[row][col +
+                                        1] if col < col_count - 1 else math.inf
+                    matrix[row][col] = min(
+                        matrix[row][col], bottom + 1, right + 1)
 
         return matrix
