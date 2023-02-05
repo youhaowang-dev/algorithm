758cfe70b2fc4c8d8f785441f40717b83c53f3be
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-04 21:13:59 -0500

    MiddleoftheLinkedList

---
+++ blind 75/MiddleoftheLinkedList.py
@@ -0,0 +1,26 @@
+#
+# https://leetcode.com/problems/middle-of-the-linked-list/
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+# Example 2:
+# Input: head = [1,2,3,4,5,6]
+# Output: [4,5,6]
+# Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+# slow and fast pointers
+class MiddleoftheLinkedList:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
