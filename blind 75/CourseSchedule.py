e9b898365b426089c0efee8b74381acb53feb4b6
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-05 12:05:58 -0500

    move file

--- blind 75/CourseSchedule.py
+++ blind 75/CourseSchedule.py
@@ -1,8 +1,13 @@
 # Depth-First Search, Breadth-First Search, Graph, Topological Sort
-# Amazon 40 Google 5 Facebook 3 Intuit 3 Apple 3 TikTok 3 Uber 2 Coupang 2 Adobe 2 Palantir Technologies 2 ByteDance 2 Coinbase 2 Microsoft 9 Bloomberg 3 Oracle 2 Salesforce 2 Tesla 2 Qualtrics 2 Citadel 2 Wayfair 2 Twitch 2 Karat 6 Twilio 5 Snapchat 4 Robinhood 4 DoorDash 3 eBay 3 Splunk 3 Wish 3 Zillow 3 JPMorgan 2 HBO 2 C3 IoT 2 Nvidia 2 VMware 2 Walmart Global Tech 2 Yelp Zenefits
+# Amazon 40 Google 5 Facebook 3 Intuit 3 Apple 3 TikTok 3 Uber 2 Coupang 2 Adobe 2 Palantir Technologies 2 ByteDance 2
+# Coinbase 2 Microsoft 9 Bloomberg 3 Oracle 2 Salesforce 2 Tesla 2 Qualtrics 2 Citadel 2 Wayfair 2 Twitch 2 Karat 6
+# Twilio 5 Snapchat 4 Robinhood 4 DoorDash 3 eBay 3 Splunk 3 Wish 3 Zillow 3 JPMorgan 2 HBO 2 C3 IoT 2 Nvidia 2
+# VMware 2 Walmart Global Tech 2 Yelp Zenefits
 # https://leetcode.com/problems/course-schedule/description/
 
-# There are a total of course_count courses you have to take, labeled from 0 to course_count - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+# There are a total of course_count courses you have to take, labeled from 0 to course_count - 1.
+# You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi
+# first if you want to take course ai.
 
 # For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 # Return true if you can finish all courses. Otherwise, return false.
@@ -17,16 +22,12 @@
 # Output: false
 # Explanation: There are a total of 2 courses to take.
 # To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
-
 # All the pairs prerequisites[i] are unique.
-from collections import deque
-from typing import Dict, List
-
 
 class CourseSchedule:
     def canFinish(self, course_count: int, prerequisites: List[List[int]]) -> bool:
         pre_to_nexts: Dict[int, List[int]] = dict()
-        for course in range(0, course_count):
+        for course in range(course_count):
             pre_to_nexts[course] = list()
 
         indegrees = [0 for _ in range(0, course_count)]
