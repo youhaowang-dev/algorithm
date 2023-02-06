4251f407ac831a30f8c33269bfe53fa7debf7e2d
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-05 19:23:53 -0500

    update solution

--- blind 75/RottingOranges.py
+++ blind 75/RottingOranges.py
@@ -24,55 +24,55 @@
 # Input: grid = [[0,2]]
 # Output: 0
 # Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
-from collections import deque
-from typing import List
 
 # bfs count rounds
-
-
 class RottingOranges:
     EMPTY = 0
     FRESH = 1
     ROTTEN = 2
-    ROUND_END = (-1, -1)
 
     def orangesRotting(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0]:
             return -1
-
-        queue = deque()
-
-        fresh_count = 0
         row_count = len(grid)
         col_count = len(grid[0])
-        for r in range(row_count):
-            for c in range(col_count):
-                if grid[r][c] == self.ROTTEN:
-                    queue.append((r, c))
-                elif grid[r][c] == self.FRESH:
+
+        fresh_count = 0
+        queue = deque()
+        for row in range(row_count):
+            for col in range(col_count):
+                if grid[row][col] == self.ROTTEN:
+                    queue.append((row, col))
+                elif grid[row][col] == self.FRESH:
                     fresh_count += 1
-        queue.append(self.ROUND_END)
 
-        minutes_elapsed = -1
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        # bfs
+        minutes_count = 0
+        while queue:  # bfs
+            locations = self.get_all_locations(queue)
+            for row, col in locations:
+                for row_delta, col_delta in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                    next_row = row + row_delta
+                    next_col = col + col_delta
+                    if not (0 <= next_row < row_count):
+                        continue
+                    if not (0 <= next_col < col_count):
+                        continue
+                    if grid[next_row][next_col] == self.FRESH:
+                        grid[next_row][next_col] = self.ROTTEN
+                        fresh_count -= 1
+                        queue.append((next_row, next_col))
+
+            if queue:
+                minutes_count += 1
+
+        if fresh_count == 0:
+            return minutes_count
+
+        return -1
+
+    def get_all_locations(self, queue) -> List[Tuple[int, int]]:
+        locations = list()
         while queue:
-            # bfs round end
-            location = queue.popleft()
-            if location == self.ROUND_END:
-                minutes_elapsed += 1
-                if queue:
-                    queue.append(self.ROUND_END)
-            # bfs round
-            else:
-                row, col = location
-                for row_delta, col_delta in directions:
-                    neighbor_row = row + row_delta
-                    neighbor_col = col + col_delta
-                    if row_count > neighbor_row >= 0 and col_count > neighbor_col >= 0:
-                        if grid[neighbor_row][neighbor_col] == self.FRESH:
-                            grid[neighbor_row][neighbor_col] = self.ROTTEN
-                            fresh_count -= 1
-                            queue.append((neighbor_row, neighbor_col))
+            locations.append(queue.popleft())
 
-        return minutes_elapsed if fresh_count == 0 else -1
+        return locations
