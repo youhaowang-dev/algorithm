743fbafbe36816f6fc35c25ec33cfb824d3a129e
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-06 22:04:22 -0500

    subsets

--- blind 75/Subsets.py
+++ blind 75/Subsets.py
@@ -1,10 +1,9 @@
 # Array, Backtracking, Bit Manipulation
-# Amazon 10 Bloomberg 8 Facebook 2 Apple 2 Google 2 icrosoft 4 Adobe 4 TikTok 4 Uber 3 Twitter 3 Reddit 3 Oracle 2 Walmart Global Tech 2 Visa 2 ByteDance 9 Goldman Sachs 6 Yahoo 2 Paypal 2 eBay 2 Coupang
+# Amazon 10 Bloomberg 8 Facebook 2 Apple 2 Google 2 icrosoft 4 Adobe 4 TikTok 4 Uber 3 Twitter 3 Reddit 3 Oracle 2
+# Walmart Global Tech 2 Visa 2 ByteDance 9 Goldman Sachs 6 Yahoo 2 Paypal 2 eBay 2 Coupang
 # https://leetcode.com/problems/subsets/description/
 
-# Given an integer array nums of unique elements, return all possible
-# subsets
-#  (the power set).
+# Given an integer array nums of unique elements, return all possible subsets (the power set).
 
 # The solution set must not contain duplicate subsets. Return the solution in any order.
 
@@ -19,25 +18,21 @@
 # space O(n * 2^n) This is exactly the number of solutions for subsets multiplied by the number N of elements to keep for each subset.
 # For a given number, it could be present or absent (i.e. binary choice) in a subset solution.
 # As as result, for N numbers, we would have in total 2^N choices (solutions).
-from ast import List
-
-
 class Subsets:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = list()
+        subsets = list()
         if not nums:
-            return result
+            return subsets
 
-        subset_state = list()
-        # needed for unordered subset, each number should only be picked once
-        start_index = 0
-        self.subsets_helper(nums, subset_state, result, start_index)
+        subset = list()
+        start = 0
+        self.build_subsets(nums, start, subset, subsets)
 
-        return result
+        return subsets
 
-    def subsets_helper(self, nums, subset_state, result, start_index):
-        result.append(list(subset_state))
-        for i in range(start_index, len(nums)):
-            subset_state.append(nums[i])
-            self.subsets_helper(nums, subset_state, result, i + 1)
-            subset_state.pop()
+    def build_subsets(self, nums, start, subset, subsets):
+        subsets.append(list(subset))
+        for i in range(start, len(nums)):
+            subset.append(nums[i])
+            self.build_subsets(nums, i + 1, subset, subsets)
+            subset.pop()
