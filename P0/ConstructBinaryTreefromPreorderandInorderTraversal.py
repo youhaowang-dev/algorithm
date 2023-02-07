5cfdd0d83a66614d0644a954c3c7469af78d0a8c
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-07 10:22:30 -0500

    ConstructBinaryTreefromPreorderandInorderTraversal

--- P0/ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ P0/ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -14,67 +14,48 @@
 # Input: preorder = [-1], inorder = [-1]
 # Output: [-1]
 
-
-from typing import List, Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-# Preorder traversal follows Root -> Left -> Right, therefore, given the preorder array preorder,
-# we have easy access to the root which is preorder[0].
-# Inorder traversal follows Left -> Root -> Right, therefore if we know the position of Root,
-# we can recursively split the entire array into two subtrees.
-# time n space n
+# time n space n, partition to exclude the root
 class ConstructBinaryTreefromPreorderandInorderTraversal:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder:
+            return None
+
+        preorder_queue = collections.deque(preorder)
         inorder_val_to_index = dict()
         for i, val in enumerate(inorder):
             inorder_val_to_index[val] = i
 
-        postorder_root_index = 0
+        return self.build_tree(preorder_queue, inorder_val_to_index, 0, len(inorder)-1)
 
-        def buildTreeHelper(inorder_left, inorder_right):
-            # The nonlocal keyword is used to work with variables inside nested functions, where the variable should not belong to the inner function.
-            nonlocal postorder_root_index
-            if postorder_root_index >= len(preorder) or inorder_left > inorder_right:
-                return None
+    def build_tree(self, preorder_queue, inorder_val_to_index, left, right):
+        if left > right:
+            return None
 
-            root_val = preorder[postorder_root_index]
-            root = TreeNode(root_val)
-            inorder_root_index = inorder_val_to_index[root_val]
+        root_val = preorder_queue.popleft()
+        root = TreeNode(root_val)
+        root_index = inorder_val_to_index[root_val]
+        root.left = self.build_tree(
+            preorder_queue, inorder_val_to_index, left, root_index - 1)
+        root.right = self.build_tree(
+            preorder_queue, inorder_val_to_index, root_index + 1, right)
 
-            postorder_root_index += 1
-            root.left = buildTreeHelper(inorder_left, inorder_root_index - 1)
-            root.right = buildTreeHelper(inorder_root_index + 1, inorder_right)
-            return root
-
-        return buildTreeHelper(0, len(inorder) - 1)
+        return root
 
 
-# time n^2 space n^2
+# time n^2 space n^2, partition to exclude the root
+# preorder[1:inorder_root_index+1], preorder[inorder_root_index+1:]
 class ConstructBinaryTreefromPreorderandInorderTraversal2:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not inorder:
+        if not preorder:
             return None
 
         root_val = preorder[0]
-        if len(inorder) == 1:
-            return TreeNode(root_val)
-
         root = TreeNode(root_val)
-        root_index = inorder.index(root_val)
+        inorder_root_index = inorder.index(root_val)  # assume unique
 
-        left_preorder = preorder[1 : root_index + 1]
-        left_inorder = inorder[:root_index]
-        root.left = self.buildTree(left_preorder, left_inorder)
-
-        right_preorder = preorder[root_index + 1 :]
-        right_inorder = inorder[root_index + 1 :]
-        root.right = self.buildTree(right_preorder, right_inorder)
+        root.left = self.buildTree(
+            preorder[1:inorder_root_index + 1], inorder[:inorder_root_index])
+        root.right = self.buildTree(
+            preorder[inorder_root_index + 1:], inorder[inorder_root_index + 1:])
 
         return root
