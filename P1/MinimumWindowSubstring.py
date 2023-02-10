379371d3e4a9c0cba809d8b5fe7c8bb4c6328f7e
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-09 21:03:06 -0500

    update solution

--- P1/MinimumWindowSubstring.py
+++ P1/MinimumWindowSubstring.py
@@ -25,55 +25,45 @@
 # Since the largest window of s only has one 'a', return empty string.
 
 # sliding window
-# maintain a char_to_count dict and a required_count
-# count > 0 means target char in substr, so required_count -= 1
-# when required_count is 0, record the min window and try to move left while
+# maintain a char_to_count dict and a need_count
+# count > 0 means target char in substr, so need_count -= 1
+# when need_count is 0, record the min window and try to move left while
 
 # "abcde" find "bd", expect "bd"
-# required_count:2      target_char_to_count: {'b': 1, 'd': 1}
-# required_count:2      target_char_to_count: {'b': 1, 'd': 1, 'a': -1}
-# required_count:1      target_char_to_count: {'b': 0, 'd': 1, 'a': -1}
-# required_count:1      target_char_to_count: {'b': 0, 'd': 1, 'a': -1, 'c': -1}
-# required_count:0      target_char_to_count: {'b': 0, 'd': 0, 'a': -1, 'c': -1}    => update
-# required_count:1      target_char_to_count: {'b': 1, 'd': 0, 'a': 0, 'c': -1, 'e': -1}
+# need_count:2      char_to_count: {'b': 1, 'd': 1}
+# need_count:2      char_to_count: {'b': 1, 'd': 1, 'a': -1}
+# need_count:1      char_to_count: {'b': 0, 'd': 1, 'a': -1}
+# need_count:1      char_to_count: {'b': 0, 'd': 1, 'a': -1, 'c': -1}
+# need_count:0      char_to_count: {'b': 0, 'd': 0, 'a': -1, 'c': -1}    => update
+# need_count:1      char_to_count: {'b': 1, 'd': 0, 'a': 0, 'c': -1, 'e': -1}
 class MinimumWindowSubstring:
-    def minWindow(self, search_string: str, target: str) -> str:
-        target_char_to_count = dict()
-        for char in target:
-            target_char_to_count[char] = 1 + target_char_to_count.get(char, 0)
+    def minWindow(self, string: str, target: str) -> str:
+        if not string or not target:
+            return ""
+
+        char_to_count = collections.Counter(target)
 
         left = 0
         right = 0
         min_window = ""
-        required_count = len(target)
-
-        for right in range(len(search_string)):
-            required_count = self.update_required_count(
-                search_string, target_char_to_count, right, required_count
-            )
-
-            while required_count == 0:
+        need_count = len(target)
+        while right < len(string):
+            # make need_count == 0
+            char_to_count[string[right]] -= 1
+            if char_to_count[string[right]] >= 0:
+                need_count -= 1
+            # make need_count > 0
+            while need_count == 0:
                 window_len = right - left + 1
                 if not min_window or window_len < len(min_window):
-                    min_window = search_string[left : right + 1]
+                    min_window = string[left: right + 1]
 
-                target_char_to_count[search_string[left]] += 1
-
-                if target_char_to_count[search_string[left]] > 0:
-                    required_count += 1
+                char_to_count[string[left]] += 1
+                if char_to_count[string[left]] > 0:
+                    need_count += 1
 
                 left += 1
 
+            right += 1
+
         return min_window
-
-    def update_required_count(
-        self, search_string, target_char_to_count, right, required_count
-    ):
-        right_char = search_string[right]
-        right_char_count = target_char_to_count.get(right_char, 0)
-        if right_char_count > 0:
-            required_count -= 1
-
-        target_char_to_count[right_char] = -1 + right_char_count
-
-        return required_count
