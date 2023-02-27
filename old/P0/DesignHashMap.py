6f1434ae9cbfa918a78765e9c06f855e6ca358b5
Youhao Wang <youhaowang.dev@gmail.com>
2023-02-27 10:07:27 -0500

    DesignHashMap.py

--- old/P0/DesignHashMap.py
+++ old/P0/DesignHashMap.py
@@ -36,99 +36,28 @@ class MyHashMap:
         self.buckets = [list() for _ in range(size)]
 
     def put(self, key: int, value: int) -> None:
-        items = self.buckets[self._hash(key)]
-        for item in items:
+        bucket = self.buckets[self._hash(key)]
+        for item in bucket:
             if item.key == key:
                 item.val = value
                 return
-        items.append(Item(key, value))
+
+        bucket.append(Item(key, value))
 
     def get(self, key: int) -> int:
-        items = self.buckets[self._hash(key)]
-        for item in items:
+        bucket = self.buckets[self._hash(key)]
+        for item in bucket:
             if item.key == key:
                 return item.val
 
         return -1
 
     def remove(self, key: int) -> None:
-        items = self.buckets[self._hash(key)]
-        for i, item in enumerate(items):
+        bucket = self.buckets[self._hash(key)]
+        for i, item in enumerate(bucket):
             if item.key == key:
-                del items[i]
-        # raise KeyError("Key not found")
-
-    def _hash(self, key: int) -> int:
-        return key % self.size
-
-# There are two main issues that we should tackle, in order to design an efficient hashmap data structure:
-# 1). hash function design and 2). collision/conflict handling.
-
-
-class ListNode:
-    def __init__(self, key, val):
-        self.pair = (key, val)
-        self.next = None
-
-
-class MyHashMap:
-    DEFAULT_SIZE = 997  # prime number also for hashing
-
-    def __init__(self):
-        self.size = self.DEFAULT_SIZE
-        self.hash_to_head = [None for _ in range(self.size)]
-
-    def put(self, key, val):
-        hash = self.get_hash(key)
-        if not self.hash_to_head[hash]:
-            self.hash_to_head[hash] = ListNode(key, val)
-        else:
-            node = self.hash_to_head[hash]
-            while node.next:  # may need the tail to append
-                # try update node
-                if self.try_update(node, key, val):
-                    return
-                node = node.next
-            # try update tail
-            if self.try_update(node, key, val):
+                bucket.pop(i)
                 return
-            # not found, append
-            node.next = ListNode(key, val)
 
-    def try_update(self, node: ListNode, key: int, val: int) -> bool:
-        if node.pair[0] == key:  # found, so update and return
-            node.pair = (key, val)
-            return True
-
-        return False
-
-    def get(self, key):
-        hash = self.get_hash(key)
-        node = self.hash_to_head[hash]
-        while node:
-            if node.pair[0] == key:
-                return node.pair[1]
-            else:
-                node = node.next
-        return -1
-
-    def remove(self, key):
-        hash = self.get_hash(key)
-        node = self.hash_to_head[hash]
-        if not node:
-            return
-        if node.pair[0] == key:  # first node is the target
-            self.hash_to_head[hash] = node.next
-        else:  # two pointers same speed
-            prev = node
-            node = node.next
-            while node:
-                if node.pair[0] == key:
-                    prev.next = node.next  # remove
-                    return
-                else:
-                    node = node.next
-                    prev = prev.next
-
-    def get_hash(self, key):
+    def _hash(self, key):
         return key % self.size
---
+++ oods.md
@@ -0,0 +1,4 @@
+# hash map
+See DesignHashMap.py
+
+# 
